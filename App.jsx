08915d28ecc9b521a29b8f75820d4f5183ddd100
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-01-31 15:54:16 +0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -8,13 +8,18 @@ import {
   Modal,
   Pressable,
   FlatList,
+  SafeAreaView,
 } from "react-native";
+import { Feather } from "@expo/vector-icons";
 import { useState } from "react";
 
 export default function App() {
   const [todos, setTodos] = useState([]);
   const [value, setValue] = useState("");
   const [modalVisible, setModalVisible] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+
+  console.log(darkMode);
 
   const addTodoHandler = () => {
     if (value.length > 0) {
@@ -32,34 +37,55 @@ export default function App() {
   };
 
   return (
-    <View style={styles.mainContainer}>
-      <Text style={styles.title}>TodoApp</Text>
+    <SafeAreaView
+      style={[
+        styles.mainContainer,
+        { backgroundColor: darkMode ? "#1a171c" : "white" },
+      ]}
+    >
+      <Text style={[styles.title, { color: darkMode ? "white" : "#1a171c" }]}>
+        TodoApp
+      </Text>
       <Button title="Add new todo" onPress={() => setModalVisible(true)} />
       <Modal visible={modalVisible} animationType="slide">
-        <TextInput
-          onChangeText={(text) => setValue(text)}
-          value={value}
-          style={styles.input}
-          placeholder="Enter todo"
-        />
-        <View
-          style={{
-            alignItems: "center",
-            flexDirection: "row",
-            gap: 20,
-            justifyContent: "center",
-          }}
+        <SafeAreaView
+          style={[
+            styles.inputContainer,
+            { backgroundColor: darkMode ? "#1a171c" : "white" },
+          ]}
         >
-          <Button onPress={addTodoHandler} title="Add Todo" />
-          <Button title="Cancel" onPress={() => setModalVisible(false)} />
-        </View>
+          <TextInput
+            onChangeText={(text) => setValue(text)}
+            value={value}
+            style={[
+              styles.input,
+              {
+                borderColor: darkMode ? "white" : "#1a171c",
+                backgroundColor: darkMode ? "white" : "white",
+              },
+            ]}
+            placeholder="Enter todo"
+          />
+          <View
+            style={{
+              alignItems: "center",
+              flexDirection: "row",
+              gap: 20,
+              justifyContent: "center",
+            }}
+          >
+            <Button onPress={addTodoHandler} title="Add Todo" />
+            <Button title="Cancel" onPress={() => setModalVisible(false)} />
+          </View>
+        </SafeAreaView>
       </Modal>
       <View style={{ gap: 10, marginTop: 20 }}>
         <FlatList
           data={todos}
+          keyExtractor={(item) => item.id.toString()}
           renderItem={(todo) => (
             <View style={styles.todoContainer}>
-              <Text key={todo.item.id}>{todo.item.title}</Text>
+              <Text>{todo.item.title}</Text>
               <Pressable
                 android_ripple={{ color: "#ccc" }}
                 onPress={() => deleteTodoHandler(todo.item.id)}
@@ -70,33 +96,60 @@ export default function App() {
           )}
         />
       </View>
-    </View>
+      <View
+        style={{
+          position: "absolute",
+          bottom: 40,
+          right: 40,
+        }}
+      >
+        {darkMode ? (
+          <Pressable onPress={() => setDarkMode(!darkMode)}>
+            <Feather name="sun" size={40} color="white" />
+          </Pressable>
+        ) : (
+          <Pressable onPress={() => setDarkMode(!darkMode)}>
+            <Feather name="moon" size={40} color="black" />
+          </Pressable>
+        )}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    padding: 8,
   },
   title: {
+    marginTop: 20,
     fontSize: 40,
     marginBottom: 20,
+    textAlign: "center",
+  },
+  inputContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
   },
   input: {
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 5,
+    width: 300,
     borderWidth: 1,
     padding: 15,
     marginBottom: 20,
   },
   todoContainer: {
     flexDirection: "row",
+    marginHorizontal: 40,
     borderWidth: 1,
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderRadius: 20,
     justifyContent: "space-between",
+    marginBottom: 10,
   },
 });
