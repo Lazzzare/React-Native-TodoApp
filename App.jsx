34f5d6560ed1f844179e990d802ba40a6c2313a0
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-02-01 02:30:43 +0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,25 +1,16 @@
-import {
-  View,
-  StyleSheet,
-  Text,
-  TextInput,
-  Button,
-  TouchableOpacity,
-  Modal,
-  Pressable,
-  FlatList,
-  SafeAreaView,
-} from "react-native";
-import { Feather } from "@expo/vector-icons";
+import { StyleSheet, SafeAreaView } from "react-native";
 import { useState } from "react";
+import Title from "./components/Title";
+import AddNewTodoButton from "./components/AddNewTodoButton";
+import ModalBox from "./components/Modal/ModalBox";
+import Todos from "./components/Todos";
+import DarkLightMode from "./components/DarkLightMode";
 
 export default function App() {
   const [todos, setTodos] = useState([]);
   const [value, setValue] = useState("");
   const [modalVisible, setModalVisible] = useState(false);
   const [darkMode, setDarkMode] = useState(false);
-
-  console.log(darkMode);
 
   const addTodoHandler = () => {
     if (value.length > 0) {
@@ -43,76 +34,23 @@ export default function App() {
         { backgroundColor: darkMode ? "#1a171c" : "white" },
       ]}
     >
-      <Text style={[styles.title, { color: darkMode ? "white" : "#1a171c" }]}>
-        TodoApp
-      </Text>
-      <Button title="Add new todo" onPress={() => setModalVisible(true)} />
-      <Modal visible={modalVisible} animationType="slide">
-        <SafeAreaView
-          style={[
-            styles.inputContainer,
-            { backgroundColor: darkMode ? "#1a171c" : "white" },
-          ]}
-        >
-          <TextInput
-            onChangeText={(text) => setValue(text)}
-            value={value}
-            style={[
-              styles.input,
-              {
-                borderColor: darkMode ? "white" : "#1a171c",
-                backgroundColor: darkMode ? "white" : "white",
-              },
-            ]}
-            placeholder="Enter todo"
-          />
-          <View
-            style={{
-              alignItems: "center",
-              flexDirection: "row",
-              gap: 20,
-              justifyContent: "center",
-            }}
-          >
-            <Button onPress={addTodoHandler} title="Add Todo" />
-            <Button title="Cancel" onPress={() => setModalVisible(false)} />
-          </View>
-        </SafeAreaView>
-      </Modal>
-      <View style={{ gap: 10, marginTop: 20 }}>
-        <FlatList
-          data={todos}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={(todo) => (
-            <View style={styles.todoContainer}>
-              <Text>{todo.item.title}</Text>
-              <Pressable
-                android_ripple={{ color: "#ccc" }}
-                onPress={() => deleteTodoHandler(todo.item.id)}
-              >
-                <Text style={styles.deleteButton}>X</Text>
-              </Pressable>
-            </View>
-          )}
-        />
-      </View>
-      <View
-        style={{
-          position: "absolute",
-          bottom: 40,
-          right: 40,
-        }}
-      >
-        {darkMode ? (
-          <Pressable onPress={() => setDarkMode(!darkMode)}>
-            <Feather name="sun" size={40} color="white" />
-          </Pressable>
-        ) : (
-          <Pressable onPress={() => setDarkMode(!darkMode)}>
-            <Feather name="moon" size={40} color="black" />
-          </Pressable>
-        )}
-      </View>
+      {/* TodoTitle */}
+      <Title darkMode={darkMode} />
+      {/* TodoAddPressableButton */}
+      <AddNewTodoButton setModalVisible={setModalVisible} />
+      {/* Modal */}
+      <ModalBox
+        modalVisible={modalVisible}
+        setValue={setValue}
+        addTodoHandler={addTodoHandler}
+        setModalVisible={setModalVisible}
+        darkMode={darkMode}
+        value={value}
+      />
+      {/* AllTodos */}
+      <Todos todos={todos} deleteTodoHandler={deleteTodoHandler} />
+      {/* DarkMode */}
+      <DarkLightMode darkMode={darkMode} setDarkMode={setDarkMode} />
     </SafeAreaView>
   );
 }
@@ -120,36 +58,5 @@ export default function App() {
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-  },
-  title: {
-    marginTop: 20,
-    fontSize: 40,
-    marginBottom: 20,
-    textAlign: "center",
-  },
-  inputContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignContent: "center",
-    alignItems: "center",
-  },
-  input: {
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 5,
-    width: 300,
-    borderWidth: 1,
-    padding: 15,
-    marginBottom: 20,
-  },
-  todoContainer: {
-    flexDirection: "row",
-    marginHorizontal: 40,
-    borderWidth: 1,
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 20,
-    justifyContent: "space-between",
-    marginBottom: 10,
   },
 });
